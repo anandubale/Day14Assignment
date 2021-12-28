b096ddd4805ef61a268764db0927059f0ef6f867
anandubale <au26199@gmail.com>
2021-12-28 13:18:04 +0530

    Added Linked List Stack

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LinkedListProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.push(70);
+            linkedListStack.push(30);
+            linkedListStack.push(56);
+            linkedListStack.Display();
+
+
+        }
+    }
+}

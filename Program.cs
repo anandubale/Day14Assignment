3266e7337a8ad5ab9df8bd8590121a522259e465
anandubale <au26199@gmail.com>
2021-12-28 13:58:37 +0530

    Added Linked List Program

--- Program.cs
+++ Program.cs
@@ -6,24 +6,12 @@ namespace LinkedListProblems
     {
         static void Main(string[] args)
         {
-            /*
-            LinkedListStack linkedListStack = new LinkedListStack();
-            linkedListStack.push(70);
-            linkedListStack.push(30);
-            linkedListStack.push(56);
-            linkedListStack.pop();
-            linkedListStack.peek();
-            linkedListStack.isEmpty();
-            linkedListStack.isFull();
-            linkedListStack.Display();
-            */
-            LinkedListQueue linkedListQueues = new LinkedListQueue();
-            linkedListQueues.Enqueue(56);
-            linkedListQueues.Enqueue(30);
-            linkedListQueues.Enqueue(70);
-            linkedListQueues.Display();
-            linkedListQueues.Dequeue();
-            linkedListQueues.Display();
+            LinkedList linkedListOriginal = new LinkedList();
+            linkedListOriginal.Add(70);
+            linkedListOriginal.Add(30);
+            linkedListOriginal.Add(56);
+            linkedListOriginal.Display();
+
         }
     }
 }
